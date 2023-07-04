882c5e8e7b37012badbfe407c6cc08a10ad4149c
Franak007 <franck.pertosa@gmail.com>
2023-07-04 16:17:28 +0200

    essai {% if defined endif %} dans twig

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,9 +11,10 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
+        $firstName = 'Franck';
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
-            'firstName' => 'Franck'
+            'firstName' => $firstName
         ]);
     }
 }
